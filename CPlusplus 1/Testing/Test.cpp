7dcb03a12218c86b4bfceede688f28cb3de64da2
austinwrobertson98 <austinwrobertson98@gmail.com>
2024-09-24 17:50:37 -0500

    Testing variables

---
+++ CPlusplus 1/Testing/Test.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int main(){
+
+    // Standard character output
+    // std::cout << "This is testing my compiler" << '\n';
+    // std::cout << "This is to test the output buffer" << '\n';
+
+    // int x; // Declaration
+    // x = 5; // Set variable
+
+    // int x = 5;
+    // int y = 6;
+
+    // std::cout << x << '\n';
+    // std::cout << y << '\n';
+
+    // integers
+    int age = 25;
+    int year = 2024;
+    int days = 7;
+
+    // doubles
+    double price = 10.99;
+    double gpa = 3.2;
+    double temp = 32.5;
+
+    // single characters
+    char grade = 'A';
+    char currency = '$';
+
+    // Booleans
+    bool gameOver = false;
+    bool student = true;
+    bool forSale = true;
+
+    // String 
+    std::string name = "Austin";
+    std::string food = "My favorite food is pizza";
+    std::string address = "123 doe drive";
+
+    // user string literal
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old" << '\n';
+
+
+    return 0;
+}
