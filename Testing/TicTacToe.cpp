7e756c8b379916bcb6377acc9366aa007784024d
austinwrobertson98 <austinwrobertson98@gmail.com>
2024-10-23 09:10:56 -0500

    Finished TicTacToe

--- Testing/TicTacToe.cpp
+++ Testing/TicTacToe.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <ctime>
+#include "AsciiHeader.hpp"
+#include <stdlib.h>
 
 //Create the 2D array
 //Ask the user which spot to pick
@@ -10,7 +13,7 @@
 //Keep going until winner
 
 //Functions
-void setUserspot(std::string map[3][3])
+void setUserSpot(std::string map[3][3])
 {
     // Set flag for successful placement
     bool isSet = false;
@@ -21,31 +24,18 @@ void setUserspot(std::string map[3][3])
         int row;
         int column;
 
-        std::cout << "Which row do you choose?: " << "\n";
+        std::cout << "Which row do you choose?: ";
         std::cin >> row;
-        std::cout << "Which column do you choose?: " << "\n";
+        std::cout << "Which column do you choose?: ";
         std::cin >> column;
 
         //Addjust row, and column
         row -= 1;
         column -=1;
 
-        if(row >= 0 && row < 3 && column >= 0 && column < 3) //Check to make sure user input is valid
-        {
-            if(map[row][column] == "_") //Check to make sure the spot is blank
-            {
+        if(row >= 0 && row < 3 && column >= 0 && column < 3){ //Check to make sure user input is valid
+            if(map[row][column] == "_"){ //Check to make sure the spot is blank
                 map[row][column] = "X";
-
-                //Display Map
-                for(int i = 0; i < 3; i++)
-                {
-                    for(int j = 0; j < 3; j++)
-                    {
-                        std::cout << map[i][j] << " ";
-                    }
-                    std::cout << "\n";
-                }
-
                 isSet = true;
             }else{
                 std::cout << "That spot has already been selected. Try again" << "\n";
@@ -56,20 +46,131 @@ void setUserspot(std::string map[3][3])
     }
 }
 
+void setComputerSpot(std::string map[3][3])
+{
+    // Set flag for successful placement
+    bool isSet = false;
+
+    while (!isSet)
+   {
+        // Initialize row and column variables
+        int row;
+        int column;
+
+        //Initialize our random number generator
+        srand(time(NULL));
+
+        //Set row and column to a random number between 0 and 2
+        row = (rand() % 3);
+        column = (rand() % 3);
+
+        if(map[row][column] == "_"){ //Check to make sure the spot is blank
+            map[row][column] = "O";
+            isSet = true;
+        }else{
+            continue;
+        }
+
+    }
+}
+
+void displayMap(std::string map[3][3])
+{
+    
+    //Display Map
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            std::cout << map[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+
+}
+
+bool checkGameOver(std::string map[3][3])
+{   
+    //Check Rows
+    for(int i = 0; i < 3; i++){
+        if(map[i][0] == "X" && map[i][1] == "X" && map[i][2] == "X"){
+            std::cout << ticTacToe;
+            displayMap(map);
+            std::cout << "Game Over! You Win!";
+            return true;
+        }
+        if(map[i][0] == "O" && map[i][1] == "O" && map[i][2] == "O"){
+            std::cout << ticTacToe;
+            displayMap(map);
+            std::cout << "Game Over! You Lose!";
+            return true;
+        }
+    }
+
+    //Check Columns
+    for(int i = 0; i < 3; i++){
+        if(map[0][i] == "X" && map[1][i] == "X" && map[2][i] == "X"){
+            std::cout << ticTacToe;
+            displayMap(map);
+            std::cout << "Game Over! You Win!";
+            return true;
+        }
+        if(map[0][i] == "O" && map[1][i] == "O" && map[2][i] == "O"){
+            std::cout << ticTacToe;
+            displayMap(map);
+            std::cout << "Game Over! You Lose!";
+            return true;
+        }
+    }
+
+    //Check diagonals
+    if(map[0][0] == "X" && map[1][1] == "X" && map[2][2] == "X"){
+        std::cout << ticTacToe;
+        displayMap(map);
+        std::cout << "Game Over! You Win!";
+        return true;
+    }
+    if(map[0][0] == "O" && map[1][1] == "O" && map[2][2] == "O"){
+        std::cout << ticTacToe;
+        displayMap(map);
+        std::cout << "Game Over! You Lose!";
+        return true;
+    }
+    if(map[0][2] == "X" && map[1][1] == "X" && map[2][0] == "X"){
+        std::cout << ticTacToe;
+        displayMap(map);
+        std::cout << "Game Over! You Win!";
+        return true;
+    }
+    if(map[0][2] == "O" && map[1][1] == "O" && map[2][0] == "O"){
+        std::cout << ticTacToe;
+        displayMap(map);
+        std::cout << "Game Over! You Lose!";
+        return true;
+    }
+
+    return false;
+
+}
 
 
 int main()
 {
+    
     //Set flag
     bool GameOver = false;
     //Create multi array
     std::string map[3][3] = {{"_", "_", "_"}, {"_", "_", "_"}, {"_", "_", "_"}};
 
     while(!GameOver)
-    {
-        setUserspot(map);
+    {   
+        std::cout << ticTacToe;
+        displayMap(map);
+        setUserSpot(map);
+        displayMap(map);
+        setComputerSpot(map);
+        displayMap(map);
+        system("cls");
+        GameOver = checkGameOver(map);
     }
-    
 
     return 0;
 }
