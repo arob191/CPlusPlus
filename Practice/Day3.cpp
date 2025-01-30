30bf2e2e757ba9abb12fa2aee483423582168c68
austinwrobertson98 <austinwrobertson98@gmail.com>
2025-01-29 20:30:33 -0600

    Day3

---
+++ Practice/Day3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    const double PI = 3.14159;
+
+    cout << PI << endl;
+
+    PI = 4;
+
+    cout << PI << endl;
+
+
+    return 0;
+}
