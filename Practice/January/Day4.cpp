d73925f1351a21e90f4e6687bd9aca9c56c3660a
austinwrobertson98 <austinwrobertson98@gmail.com>
2025-01-30 20:44:24 -0600

    C++ training

---
+++ Practice/January/Day4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using std::string, std::cout, std::cin, std::endl; 
+
+
+int main() {
+    // Define variables
+    string username;
+
+    cout << "Hello. What is your name?" << std::endl;
+    getline(cin, username);
+
+    while(username.empty()) {
+        
+        cout << "What is your name? " << endl;
+        getline(cin, username);
+
+
+        if (username.empty()){
+            cout << "Please provide a name" << endl;
+        }
+
+    }
+    
+    cout << "Hello " << username << endl;
+
+    return 0;
+}
